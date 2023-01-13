7418b2cd5f8fe6a24a1b49c3967a20e0be8d8a4d
FreeMan-92 <freeman19920202@gmail.com>
2023-01-13 18:45:13 +0300

    Add files via upload

---
+++ Practice.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <windows.h>
+
+void function()
+{
+    for (;;);
+}
+
+std::vector<std::thread> kros;
+int main()
+{
+    for (unsigned int i = 0; i < 10; ++i)
+    {
+        std::cout << "Process ID: " << i << "\n";
+        kros.emplace_back(function);
+        SetThreadAffinityMask(kros.back().native_handle(), DWORD_PTR(1) << i);
+    }
+    for (auto& ThreadFunction : kros)
+    {
+        std::cout << "CPU Cores are working... Check your Application Manager... If you can..." << "\n";
+        ThreadFunction.join();
+    }
+        
+}
